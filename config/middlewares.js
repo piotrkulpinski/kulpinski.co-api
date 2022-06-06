3112f74de4fe062182ef151e17d775960299d555
Piotr Kulpinski <piotr@kulpinski.pl>
2022-06-06 21:34:55 +0200

    Add AWS S3 provider

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,4 +9,30 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            's3.eu-west-3.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            's3.eu-west-3.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
